8edaf15579c2b5f9e5807481dd116cb903aeae08
janek <oskroba@student.agh.edu.pl>
2026-01-12 03:57:33 +0100

    male poprawki komentarzy i printow, prep do further work

--- src/projekt.py
+++ src/projekt.py
@@ -39,8 +39,9 @@ S_okien = 2*1.8*1.95
 R_a2_okna = izol_zewn_ra2(L_zewn, L_wewn, S_okien, a)
 
 print(f"POKOJ BIUROWY 001:\n\
-      izolacyjność przegrody pełnej: {R_a2_zelbet}\n\
-      izolacyjność przeszklenia: {R_a2_okna}")
+      Wymagania PN-B-02151-3_2015-10P, TAB 7, p 9.1 (s.38): Laeq_zewn = 40dB dla pom. biurowych\n\
+izolacyjność przegrody pełnej: {R_a2_zelbet}\n\
+izolacyjność przeszklenia: {R_a2_okna}")
 #########################################################################
 
 #####--------GRUBOŚĆ PRZEGRODY PEŁNEJ POK 001--------###
@@ -50,16 +51,16 @@ masa_pow = np.pow(10,(R_a2_zelbet + 29.6)/30.9)
 S_beton = S_scian_zewn - S_okien
 grubosc_beton = masa_pow/rho_beton
 print(f"GRUBOSC PRZEGRODY PELNEJ ZEWN. : {np.round(grubosc_beton, 2)} [m]")
-
+print('#######')
 ########################################################
 
 ###---ściana 001 na korytarz
 dlugosc_sciany = 3.47 + 6.5 - 4.89
-print(dlugosc_sciany)
+# print(dlugosc_sciany)
 wild_guess = 0.10                                                   ######<------check check
 ms = rho_beton * wild_guess
 R_a1r = 30.9*np.log10(ms) - 26.1
-print(R_a1r)
+#print(R_a1r)                                #####<-- roboczy print do zgadywaniaa
 
 #poprawka przen. bocz: (STG)
 Ka = 1                                                              #####<-------check check
@@ -77,7 +78,14 @@ R_a1r = R_a1 - 2 # ~~60
 Ka = 9 # strona 117 zalacznik cz. II.3 
 R_a1r_prim = R_a1r - Ka #wczesniej nie było potrzeby nazywania zmiennej z primem, zbyt dluga nazwa
 #poprawka przen. bocz: (STG)
-print(f"Izolacyjność przegr. wewn systemowej 001/002:  {np.round(R_a1r_prim, 2)} dBA, wymaganie 50")
+print(f"Izolacyjność przegr. wewn systemowej 001/002:  {np.round(R_a1r_prim, 2)} dBA, wymaganie 50 - TAB 5., VIII.3.1 (s.26)")
+print("proponowany system: 3.41.03, wariant 2x100mm ")
 
 
-###Dla tego pokoju zostało tylko policzyć grubość betonowej wesnętrznej z lab 2
+###Dla tego pokoju zostało tylko policzyć grubość betonowej wesnętrznej z lab 2 
+# --- komment z 12.01:  ???? ju jest napisana grubość 0.1 wiec nie wiem o co tu chodziło
+                        ##coś gadaliśmy z Matim Gajewskim ze lepiej zgadywać niz wyliczac ale nie pamietam dokladnie
+
+###TODO jeszcze trzeba dla 001 policzyc przegrode zewn. bez okien. berdzie inna grubosc. nie dodajemy poprawki na okno.
+
+print("\n\n--- KONIEC POKOJU 001---\n\n")
